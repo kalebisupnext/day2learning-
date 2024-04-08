6112414cdc63dd7dcfb7a15029b9c5e4b750ef0a
Svenningsen <ksvenningse@cps.k12.il.us>
2024-04-08 13:22:27 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# #order of execution 
+# # in pyton
+# print("hello world")
+# #stings are surronded by quotes
+# #single or dubble quotes 
+# #be consittent with the quoutes you use 
+# print("order of execition")
+# print("in python")
+# print("*"*20)
+
+
+# #variables are used to store data 
+# #variables are created when you assign a value to it 
+# #variables are case senstive 
+# price=10 #string variable 
+# name="john"#string variable 
+# rating=4.9 #float variable 
+# is_published = True #boolen variable 
+# #start all variables with a lowercase letter or underscore 
+# #use underscore to seprate words 
+# #use camel case if you want to starts with capatl letter 
+# #on the next world 
+# playerBulls="michael jordan"
+# #concaatnation to join variables in a sentence 
+# print(name + "is a basketball player")
+# print(name + "has a ring of" + str(rating))
+# # use the str function to convert a number to a sting 
+# print("the price of the book is  "+str(price))
+# #the plus sign is used to concaateenate strings 
+
+#getting input from the user 
+
+
+#ask twi questions from the user 
+# persons name and two favroite colors 
+#then print a message like "moses like blue "
+
+name= ("what is your name?")
+color=("what is your favroite color")
+print= (name +"likes" +color)
